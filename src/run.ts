bc1069c0e8cd69fa57aa47d3638460d5ca72d205
Rick Love <rick@toldpro.com>
2020-04-24 09:43:39 -0500

    Add Large File Calls

--- src/run.ts
+++ src/run.ts
@@ -6,8 +6,10 @@ export const fs = {
     appendFile: util.promisify(fsRaw.appendFile),
 };
 
-const TIMEOUT = 5000;
+const TIMEOUT = 10000;
 const TIME_WARN = 500;
+const KBPS_WARN = 1000000;
+const TICK_TIME = 1000;
 const SETTINGS = { outDir: `../reports` };
 
 const appendToFile = async (text: string) => {
@@ -40,40 +42,60 @@ const tryFetch = async (name: string, url: string, timeout = TIMEOUT) => {
 
     try {
         const response = await fetch(url, { timeout });
+        const blob = await response.blob();
         const timeDeltaMs = getTimeSinceMs(timeStart);
-        const message = `ms=${(timeDeltaMs + '').padStart(6, ' ')} \t${name} \t${url}`;
+        const size = blob.size;
+        const bps = (size * 1000 / timeDeltaMs) | 0;
+        const kbps = bps / 1000;
+        const message = `ms=${(timeDeltaMs + '').padStart(6, ' ')} \tsize=${(size + '').padStart(9, ' ')} \tkbps=${(kbps + '').padStart(8, ' ')} \t${name} \t${url}`;
 
         if (!response.ok) {
-            logError(`ERROR \t${message} \t${response.status} \t${response.statusText}`);
-        } else if (timeDeltaMs > TIME_WARN) {
+            logError(`${message} \tERROR RESPONSE \t${response.status} \t${response.statusText}`);
+        } else if (timeDeltaMs > TIME_WARN && kbps < KBPS_WARN) {
             logWarning(message);
         } else {
             logInfo(message);
         }
     } catch (err) {
-        const message = `ms=${(timeout + '').padStart(6, ' ')} \t${name} \t${url}`;
+        const timeDeltaMs = getTimeSinceMs(timeStart);
+        const message = `ms=${(timeDeltaMs + '').padStart(6, ' ')} \t${name} \t${url} \tUNKNOWN ERROR `;
         logError(message);
     }
 };
 
-const loop = async () => {
+const calls = [
+    { name: 'GoogleFavIcon', url: 'http://www.google.com/images/google_favicon_128.png' },
+    { name: 'Bing__FavIcon', url: 'http://www.bing.com/s/a/bing_p.ico' },
+    { name: 'AmazonFavIcon', url: 'https://www.amazon.com/favicon.ico' },
+    { name: 'Apple_FavIcon', url: 'https://www.apple.com/favicon.ico' },
+    { name: 'WIFI__FavIcon', url: 'http://192.168.1.1/0.1/gui/images/faviconNone.ico' },
+];
 
+const largeCalls = [
+    { name: 'ReactDomJsCDN', url: 'https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js' },
+    { name: 'AmazonImageLG', url: 'https://images-na.ssl-images-amazon.com/images/S/amazonlive-media-prod/broadcast/3/3954005c-5c84-4157-b6a4-567710c59414_20200423130720/image/slate.jpg' },
+    { name: 'AppleScriptLG', url: 'https://www.apple.com/metrics/ac-analytics/2.9.0/scripts/ac-analytics.js' },
+    { name: 'Azure_ImageLG', url: 'https://azurecomcdn.azureedge.net/cvt-7e63ee798fb5c57c26a10e5149d1055e6600c12efc783ef793b34b8d06646c40/images/page/home/customer-tabs/cincinnati-childrens-desktop.jpg' },
+    { name: 'GoogleImageLG', url: 'https://ssl.gstatic.com/gb/images/p1_6269e604.png' },
+    { name: 'YoutubeScript', url: 'https://www.youtube.com/yts/jsbin/desktop_polymer_inlined_html_polymer_flags_v2-vflGtoGN1/desktop_polymer_inlined_html_polymer_flags_v2.js' },
+];
+
+const allCalls = largeCalls.flatMap(x => [x, ...calls]);
+
+const loop = async (i: number) => {
     console.log(`${new Date().toISOString()}`);
-    const calls = [
-        tryFetch('GoogleFavIcon', 'http://www.google.com/images/google_favicon_128.png'),
-        tryFetch('Bing__FavIcon', 'http://www.bing.com/s/a/bing_p.ico'),
-        tryFetch('WIFI__FavIcon', 'http://192.168.1.1/0.1/gui/images/faviconNone.ico'),
-    ];
 
-    await Promise.all(calls);
+    const call = allCalls[i % allCalls.length];
+    await tryFetch(call.name, call.url);
 };
 
 export const run = async (outDir: string) => {
     SETTINGS.outDir = path.resolve(outDir);
 
+    let i = 0;
     setInterval(() => {
-        try { loop(); } catch{ }
-    }, TIMEOUT);
+        try { loop(i++); } catch{ }
+    }, TICK_TIME);
 };
 
 // run();
